84dad069ebe42f9223a10e49f4b30fce68e5a5bc
Miguel Ángel Ruiz <miguelangel.ruiz@m-bet.com>
2023-05-26 01:24:20 +0200

    Tweaked phpunit.bat.

--- cmd/phpunit.bat
+++ cmd/phpunit.bat
@@ -5,5 +5,6 @@ SET "CURRENTPATH=%CURRENTPATH:\=/%"
 :: SET CURRENTPATH=C:/DESARROLLO/inventario_sotano_api2
 
 :: docker exec phpunit run %*
-cmd/php vendor/bin/phpunit --configuration phpunit.xml %*
+:: cmd/php vendor/bin/phpunit --configuration phpunit.xml.dist %*
 :: docker run --rm --interactive --tty --volume %CURRENTPATH%:/app phpunit/phpunit run %*
+docker-compose exec php php vendor/bin/phpunit --configuration phpunit.xml.dist %*
